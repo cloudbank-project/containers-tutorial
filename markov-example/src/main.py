2dbc1e8673278bc2e562bc1b089ef7ba23e93b8e
Naomi Alterman <naomila@uw.edu>
2025-03-21 14:30:02 -0700

    Update markov script

--- markov-example/src/main.py
+++ markov-example/src/main.py
@@ -1,12 +1,13 @@
 import argparse
 import os
+import sys
 
 import markovify
 
 
 def load_training_data(filename):
     lines = []
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="utf8") as f:
         for line in f:
             if len(line) > 20:
                 lines.append(line)
@@ -19,6 +20,8 @@ if __name__ == "__main__":
                         help="how many sentences to output")
     parser.add_argument("--pdf", metavar="FILENAME", type=str,
                         help="output as a pdf file")
+    parser.add_argument("--txt", metavar="FILENAME", type=str,
+                        help="output as a plaintext file")
     args = parser.parse_args()
 
     data = load_training_data("data/mechanics-of-materials.txt")
@@ -29,7 +32,13 @@ if __name__ == "__main__":
         out_text.append(model.make_sentence(tries=100))
     out_text = " ".join(out_text)
 
-    if args.pdf is None:
+    if args.pdf is None and args.txt is None:
         print(out_text)
-    else:
+        sys.exit(0)
+
+    if args.pdf is not None:
         os.system("echo \"%s\" | enscript -p - | ps2pdf - %s" % (out_text, args.pdf))
+    if args.txt is not None:
+        with open(args.txt, "w") as f:
+            f.write(out_text)
+    sys.exit(0)
